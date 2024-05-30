d811b8e02582df99e7dec7f6b318d0294f422f99
mamedovsh <shagin.mamedoff@yandex.ru>
2024-05-30 19:01:16 +0700

    Задачи семинар 4

---
+++ Seminar4/script.js
@@ -0,0 +1,35 @@
+// Задача 1:
+// Необходимо получить список всех пользователей с помощью бесплатного API
+// (https://jsonplaceholder.typicode.com/users) и отобразить их на странице.
+// Пользователь должен иметь возможность удалить любого пользователя из списка.
+
+// const myPromise = new Promise((resolve, reject) => {});
+
+// myPromise.then((value) => {}).catch((error) => console.log("ошибка"));
+
+// const url1 = "https://jsonplaceholder.typicode.com/users";
+
+// const getData = async (argUrl) => {
+//   try {
+//     const res = await fetch(argUrl);
+//     const data = await res.json();
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+// const fetchData = await getData(url1);
+// console.log(fetchData);
+
+// const deleteBtn = document.querySelectorAll('.btn__del');
+//     deleteBtn.forEach((button) => {
+//       button.addEventListener('click', () => {
+//         const product = button.closest('.card');
+//         product.remove();
+
+//       })
+//     });
+
+// Необходимо реализовать отрисовку 10 картинок 
+// собак из API https://dog.ceo/dog-api/ с интервалом в 3 секунды.
+
